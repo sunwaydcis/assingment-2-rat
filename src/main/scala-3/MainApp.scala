6dea21842b96362910b8319fc7e5047084c4553f
timkwek <22078620@imail.sunway.edu.my>
2025-11-29 17:30:19 +0800

    Create class and map constants
    Create Booking record class and map constants to CSV header names

--- src/main/scala-3/MainApp.scala
+++ src/main/scala-3/MainApp.scala
@@ -1,12 +1,35 @@
-//import external library for csv
 import java.io.File
+//import external library for csv
 import com.github.tototoshi.csv
 import com.github.tototoshi.csv.CSVReader
+import scala.util.Try
+import scala.collection.mutable
+
+//case class to represent a single hotel booking record
+case class Booking(
+  //only use the columns needed for analysis
+  originCountry: String,
+  hotelName: String,
+  bookingPrice: Double,
+  discount: Double,
+  profitMargin: Double,
+  visitors: Int
+)
+
+object HotelData {
+  //map constants to CSV header names
+  val ORIGIN_COUNTRY = "Origin Country"
+  val HOTEL_NAME = "Hotel Name"
+  val BOOKING_PRICE = "Booking Price[SGD]"
+  val DISCOUNT = "Discount"
+  val PROFIT_MARGIN = "Profit Margin"
+  val VISITORS = "No. Of People"
 
 //test - read csv file and output rows
 object MainApp {
   def main(args: Array[String]): Unit = {
 
+    //define filepath to dataset
     val filePath = "/Users/tim/Downloads/Hotel_Dataset.csv"
 
     //use CSVReader to handle parsing of csv file
