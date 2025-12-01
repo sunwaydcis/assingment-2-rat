ecb77160af4eb60dfb989416d33a4f63e8560bb2
roshannn0 <roshanhangouts@gmail.com>
2025-12-01 11:37:31 +0800

    Updated variables
    Added variables to be extracted from the dataset to answer the questions

--- src/main/scala-3/MainApp.scala
+++ src/main/scala-3/MainApp.scala
@@ -8,22 +8,30 @@ import scala.collection.mutable
 //case class to represent a single hotel booking record
 case class Booking(
   //only use the columns needed for analysis
+  bookingID: String,
   originCountry: String,
   hotelName: String,
   bookingPrice: Double,
   discount: Double,
   profitMargin: Double,
-  visitors: Int
+  visitors: Int,
+  destinationCountry: String,
+  noOfDays : Int,
+  rooms: Int
 )
 
 object HotelData {
   //map constants to CSV header names
+  val BOOKING_ID = "Booking ID"
   val ORIGIN_COUNTRY = "Origin Country"
   val HOTEL_NAME = "Hotel Name"
   val BOOKING_PRICE = "Booking Price[SGD]"
   val DISCOUNT = "Discount"
   val PROFIT_MARGIN = "Profit Margin"
   val VISITORS = "No. Of People"
+  val DESTINATION_COUNTRY = "Destination Country"
+  val NO_OF_DAYS = "No of Days"
+  val ROOMS = "Rooms"
 
   //read csv file and parse columns into Booking case class then returns a list of successfully parsed Booking objects
   def loadHotelDataset(filePath: String): List[Booking] = {
@@ -39,13 +47,19 @@ object HotelData {
           discount <- Try(row(DISCOUNT).toDouble).toOption
           profit <- Try(row(PROFIT_MARGIN).toDouble).toOption
           visitors <- Try(row(VISITORS).toInt).toOption
+          noOfDays <- Try(row(NO_OF_DAYS).toInt).toOption
+          rooms <- Try(row(ROOMS).toInt).toOption
         } yield Booking(
+          bookingID = row(BOOKING_ID).trim,
           originCountry = row(ORIGIN_COUNTRY).trim,
           hotelName = row(HOTEL_NAME).trim,
           bookingPrice = price,
           discount = discount,
           profitMargin = profit,
-          visitors = visitors
+          visitors = visitors,
+          destinationCountry = row(DESTINATION_COUNTRY).trim,
+          noOfDays = noOfDays,
+          rooms = rooms
         )
 
         if (result.isEmpty) {
